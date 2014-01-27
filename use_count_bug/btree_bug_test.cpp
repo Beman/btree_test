60cdbe52ba72b16ea9924c6a53231bd15fb7c4ca
Beman <bdawes@acm.org>
2014-01-27 15:27:30 -0500

    Naked new's not being captured in some form of smart pointer is considered very bad form because so often, as is the case here, it is a sign of exception unsafe code. Doing unnecessary dynamic allocation is also considered a bad habit, as it is much less efficient and harder to debug than automatic allocation.
    The usual recommendation from Stroustrup and other teachers of the language is to create temporary variables just before they are used.
    
    Applying those rules results in a program that happens to workaround the iterator lifetime problem!

--- use_count_bug/btree_bug_test.cpp
+++ use_count_bug/btree_bug_test.cpp
@@ -9,33 +9,31 @@
 static const char temp_file[] = "temp_file";
 
 int cpp_main(int argc, char* argv[]) {
-  typedef map_btree_t<uint64_t, uint64_t> map_t;
 
-  map_t* map;
-  size_t size __attribute__((unused));
-  map_t::map_const_iterator_bool_pair it_bool_pair; 
+  typedef map_btree_t<uint64_t, uint64_t> map_t;
 
   // clean up from any previous run
   remove(temp_file);
 
-  // create new map
-  map = new map_t(temp_file, RW_NEW);
+  {
+    // create new map
+    map_t map(temp_file, RW_NEW);
 
-  // check count
-  size = map->size();
+    // check count
+    size_t size __attribute__((unused));
+    size = map.size();
 
-  // change entry invalid
-  it_bool_pair = map->change(6000006, 60);
+    // change entry invalid
+    map_t::map_const_iterator_bool_pair it_bool_pair = map.change(6000006, 60);
 
-  // check count stayed same
-  size = map->size();
+    // check count stayed same
+    size = map.size();
+  }
 
-  // end RW tests
-  delete map;
-
-  // open and reclose Read Only
-  map = new map_t(temp_file, READ_ONLY);
-  delete map;
+  {
+    // open and reclose Read Only
+    map_t map(temp_file, READ_ONLY);
+  }
 
   std::cout << "Almost done.\n";
 
